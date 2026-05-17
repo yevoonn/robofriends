bae7760bb5fa84785646319a22f14f6e24fc9e73
Przemysław Żabkiewicz <yevoonn@gmail.com>
2026-05-17 23:05:28 +0200

    vite config update - setting base to empty string

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/robofriends/',
+  base: "",
   css: {
     lightningcss: {
       errorRecovery: true, // This strips out invalid/outdated CSS hacks
