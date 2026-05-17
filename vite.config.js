a2b12b55d95a1f7564fa41dbb6bc3bd3e0fd4b28
Przemysław Żabkiewicz <yevoonn@gmail.com>
2026-05-17 22:38:28 +0200

    replacing vite template with actual project files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    lightningcss: {
+      errorRecovery: true, // This strips out invalid/outdated CSS hacks
+    },
+  },
 })
